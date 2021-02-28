730ec9411a959c8779312f24038eae2846f5ef2e
Erdem Tombaş <erdemtombas@gmail.com>
2021-03-01 01:04:23 +0300

    Add project files.

---
+++ DataAccsess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccsess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccsess.Abstract
+{
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+    }
+}
+
+//Code Refactoring
---
+++ DataAccsess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,33 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccsess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccsess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
+    {
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             { 
+                                 ProductId = p.ProductId, ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
+    }
+}
